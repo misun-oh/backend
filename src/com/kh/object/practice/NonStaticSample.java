14d02501769280c4c435f3bb5c0d8f4db1c25779
misun <momo.class55@gmail.com>
2026-07-27 10:11:51 +0900

    메서드 연습

---
+++ src/com/kh/object/practice/NonStaticSample.java
@@ -0,0 +1,22 @@
+// 패키지 선언부
+package com.kh.object.practice;
+
+// 클래스 선언부
+public class NonStaticSample {
+    // 필드
+    
+    // 메서드 선언부
+    // 접근제한자 반환타입 메서드명(매개변수타입 매개변수명){}
+    public void printLottoNumber(){
+        
+    }
+
+    public void outputChar(int num, char c){
+        // return ""
+        // return ''
+        // return 0
+    }
+    // 생성자
+
+    
+}
